041f6330185ecec766fba300b16de410e4b94fb1
Mates0 <73097527+Mates0@users.noreply.github.com>
2021-04-29 16:03:57 +0200

    Add files via upload

---
+++ src/main/java/cz/educanet/minesweeper/logic/Area.java
@@ -0,0 +1,22 @@
+package cz.educanet.minesweeper.logic;
+
+public class Area {
+    private int state;
+    private boolean bomb;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public boolean getBomb() {
+        return bomb;
+    }
+
+    public void setBomb(boolean bomb) {
+        this.bomb = bomb;
+    }
+}
